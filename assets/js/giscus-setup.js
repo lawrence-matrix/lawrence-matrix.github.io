565c3cca69f5a0424bc21a7b8f3af122b07bac74
lawrence-matrix <146606119+lawrence-matrix@users.noreply.github.com>
2026-05-14 04:42:23 +0000

    Deploying to gh-pages from @ lawrence-matrix/lawrence-matrix.github.io@7ed07663cbe6b67c49f0048472b5a30fdbfb4629 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "lawrence-matrix/personal-website",
+    "data-repo": "lawrence-matrix/lawrence-matrix.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
